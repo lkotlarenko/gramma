b28fb7c01b41d1f2e61821c128a1ac4ed6ddc045
Elly <lkotlarenko@proton.me>
2024-05-21 11:46:38 -0300

    refactor: 🚚 move commands to a different file

---
+++ src/config.py
@@ -0,0 +1,6 @@
+APP_NAME = "Gramma"
+APP_ICON = "src/images/app_icon.ico"
+TRAY_ICON = "src/images/tray_icon.png"
+NOTIFICATION_TIMEOUT_SHORT = 2
+NOTIFICATION_TIMEOUT_LONG = 4
+ERR_ALREADY_RUNNING = "Another instance of Gramma is already running!"
