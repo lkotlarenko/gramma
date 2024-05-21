1a25a9f80f376138f9ad90d035769fdd081f0ca7
Elly <lkotlarenko@proton.me>
2024-05-21 13:09:52 -0300

    refactor: 🎨 clean and format main script

--- gramma.py
+++ gramma.py
@@ -18,7 +18,6 @@ from src.config import (
     TRAY_ICON,
     NOTIFICATION_TIMEOUT_SHORT,
     NOTIFICATION_TIMEOUT_LONG,
-    ERR_ALREADY_RUNNING,
 )
 
 if sys.platform.startswith("win"):
@@ -35,15 +34,9 @@ class ClipboardListener:
         self.last_called = 0
         self.client = AsyncGroq(api_key=self.GROQ_API_KEY)
         self.commands = self.load_commands()
-
-        # Ensure only one instance is running
-        if sys.platform.startswith("win"):
-            self.handle_mutex()
-        else:
-            self.handle_lock_file()
+        self.ensure_single_instance()
 
     def load_env_vars(self) -> None:
-        # Load environment variables .env file.
         load_dotenv()
         self.GROQ_API_KEY = os.getenv("GROQ_API_KEY")
         self.GROQ_MODEL = os.getenv("GROQ_MODEL")
@@ -56,11 +49,9 @@ class ClipboardListener:
             app_icon=APP_ICON,
             timeout=NOTIFICATION_TIMEOUT_LONG,
         )
-        # Load commands from commands.py.
         return commands
 
     async def process_command(self, command: str, user_msg: str) -> None:
-        # Process a command and send the response to the clipboard.
         if time.time() - self.last_called < 4:
             self.notify(
                 "Rate Limit",
@@ -99,11 +90,9 @@ class ClipboardListener:
             self.notify("Error", f"An error occurred: {e}", NOTIFICATION_TIMEOUT_LONG)
 
     def process_command_sync(self, command: str, user_msg: str) -> None:
-        # Run the asynchronous process_command method synchronously.
         asyncio.run(self.process_command(command, user_msg))
 
     def notify(self, title: str, message: str, timeout: int) -> None:
-        # Display a notification.
         notification.notify(
             title=title,
             message=message,
@@ -113,7 +102,6 @@ class ClipboardListener:
         )
 
     def monitor_clipboard(self) -> None:
-        # Continuously monitor the clipboard for new commands.
         recent_value = pyperclip.paste()
         while True:
             time.sleep(0.1)
@@ -123,13 +111,10 @@ class ClipboardListener:
                 for prefix in self.commands:
                     if tmp_value.startswith(f"!{prefix.lower()[7:]}"):
                         command = tmp_value.split(" ")[0][1:]
-                        user_msg = tmp_value[
-                            len(command) + 2 :
-                        ].strip()  # +2 to account for space and '!'
+                        user_msg = tmp_value[len(command) + 2 :].strip()
                         self.process_command_sync(command, user_msg)
 
     def setup_tray_icon(self) -> None:
-        # Setup the system tray icon.
         image = Image.open(TRAY_ICON)
         icon = pystray.Icon(
             APP_NAME,
@@ -140,41 +125,31 @@ class ClipboardListener:
         icon.run()
 
     def exit_program(self, icon) -> None:
-        # Exit the program.
         icon.stop()
         os._exit(0)
 
-    def handle_mutex(self):
-        # Ensure single instance on Windows using a mutex.
+    def ensure_single_instance(self) -> None:
+        if sys.platform.startswith("win"):
+            self.handle_mutex()
+        else:
+            self.handle_lock_file()
+
+    def handle_mutex(self) -> None:
         self.mutex = None
         try:
             self.mutex = win32event.CreateMutex(None, False, APP_NAME)
             if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
-                notification.notify(
-                    title=APP_NAME,
-                    message=ERR_ALREADY_RUNNING,
-                    app_name=APP_NAME,
-                    app_icon=APP_ICON,
-                    timeout=NOTIFICATION_TIMEOUT_LONG,
-                )
-                os._exit(1)
+                self.notify_instance_already_running()
         except Exception as e:
             print(f"Error creating mutex: {e}")
             os._exit(1)
 
-    def handle_lock_file(self):
-        # Ensure single instance on Unix using a lock file.
+    def handle_lock_file(self) -> None:
         lock_file = "/tmp/clipboardlistener.lock"
         try:
             self.lock_fd = os.open(lock_file, os.O_CREAT | os.O_EXCL | os.O_RDWR)
         except OSError:
-            notification.notify(
-                title=APP_NAME,
-                message=ERR_ALREADY_RUNNING,
-                app_name=APP_NAME,
-                app_icon=APP_ICON,
-                timeout=NOTIFICATION_TIMEOUT_LONG,
-            )
+            self.notify_instance_already_running()
             os._exit(1)
         else:
             fcntl.flock(self.lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -185,7 +160,6 @@ if __name__ == "__main__":
     threading.Thread(target=listener.setup_tray_icon).start()
     listener.monitor_clipboard()
 
-    # Release the lock/mutex when the script exits
     if sys.platform.startswith("win"):
         win32event.ReleaseMutex(listener.mutex)
     else:
